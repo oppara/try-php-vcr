f99892c706afd0cb5f55cf77d8bd4388df632911
oppara <oppara@oppara.tv>
2021-03-16 02:25:25 +0900

    initial commit

---
+++ README.md
@@ -0,0 +1,16 @@
+# try-php-vcr
+
+[php-vcr/php-vcr](https://github.com/php-vcr/php-vcr) と [php-vcr/phpunit-testlistener-vcr](https://github.com/php-vcr/phpunit-testlistener-vcr) の動作確認。
+
+
+## Usage
+
+```
+$ composer install
+$ composer test
+```
+
+
+## Reference
+
+[PHP-VCR が curl 関数を乗っ取る方法 | NEW GAME @kunit](https://kunit.jp/archives/108)
---
+++ tests/TestCase/VCRTest.php
@@ -0,0 +1,62 @@
+<?php
+declare(strict_types=1);
+
+use PHPUnit\Framework\TestCase;
+use VCR\VCR;
+
+class VCRTest extends TestCase
+{
+    public const URL = 'https://www.google.com';
+
+    /**
+     * @test
+     * @vcr file_get_contents
+     */
+    public function fielGetContentsRequest(): void
+    {
+        $expected = file_get_contents(static::URL);
+        $this->assertSameBody($expected);
+    }
+
+    /**
+     * @test
+     * @vcr curl
+     */
+    public function curlRequest(): void
+    {
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, static::URL);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        $expected = curl_exec($ch);
+        curl_close($ch);
+
+        $this->assertSameBody($expected);
+    }
+
+    protected function getCasseteName(): string
+    {
+        $class = \get_class($this);
+        $method = $this->getName(false);
+        $reflection = new \ReflectionMethod($class, $method);
+        $docBlock = $reflection->getDocComment();
+
+        $tag = '@vcr';
+        $regex = "/{$tag} (.*)(\\r\\n|\\r|\\n)/U";
+        preg_match_all($regex, $docBlock, $matches);
+
+        return $matches[1][0] ?? '';
+    }
+
+    protected function assertSameBody(string $expected): void
+    {
+        $cassetteName = $this->getCasseteName();
+        $yamlObjs = new \VCR\Storage\Yaml(dirname(__DIR__) . '/fixtures', $cassetteName);
+        $actual = [];
+        foreach ($yamlObjs as $obj) {
+            $actual[] = $obj;
+        }
+
+        $body = $actual[0]['response']['body'];
+        $this->assertSame($expected, $body);
+    }
+}
